7c5b79390a1e05e0a14fb366d292508e48108dcf
FAHIMABBAS HARIYANI <fheemabbas@gmail.com>
2021-01-23 02:14:43 +0530

    add text feilds

--- src/App.js
+++ src/App.js
@@ -4,7 +4,23 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>hello World</h1>
+      <h1>Data App</h1>
+      <input
+        className="input"
+        type="text"
+        name="name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <textarea
+        className="input"
+        name="address"
+        rows="4"
+        onChange={(e) => setAddress(e.target.value)}
+        cols="22"
+      />
+      <br />
+      <button type="submit">submit</button>
     </div>
   );
 }
